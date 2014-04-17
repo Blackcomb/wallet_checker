1ef9cd21ba7e9d8f09a86ee275f48bd939a02355
arcoard <arcoard+github@gmail.com>
2014-04-17 14:13:10 -0700

    Added AngularJS, and a fn to query Blockchain.info

--- assets/js/lookup.js
+++ assets/js/lookup.js
@@ -1,1 +1,12 @@
 var lookupApp = angular.module('lookupApp', []);
+
+glob = '';
+
+lookupApp.controller('mainPageCtrl', function($scope, $http){
+	$scope.lookupAddress = function(address){
+		var url = 'https://blockchain.info/multiaddr?cors=true&active='+address;
+		$http.get(url).success(function(data){
+			glob = data;
+		});
+	}
+});
