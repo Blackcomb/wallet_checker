26591d9f1125a2d80d806ce96ceecd006a64b4a3
arcoard <arcoard+github@gmail.com>
2014-04-17 15:45:22 -0700

    Display Blockchain.info response dynamically in webpage.

--- assets/js/lookup.js
+++ assets/js/lookup.js
@@ -3,10 +3,20 @@ var lookupApp = angular.module('lookupApp', []);
 glob = '';
 
 lookupApp.controller('mainPageCtrl', function($scope, $http){
-	$scope.lookupAddress = function(address){
+	$scope.loaded = false;
+
+	$scope.lookupAddress = function(address){		
+		// $scope.loaded = false;
 		var url = 'https://blockchain.info/multiaddr?cors=true&active='+address;
 		$http.get(url).success(function(data){
+			$scope.loaded = true;
 			glob = data;
+			$scope.output = {
+				'BTC' : data.wallet.final_balance / 100000000, //Response in satoshi, so have to divide.
+				'Address' : address,
+				'Total Received': data.addresses[0].total_received,
+				'Total Sent': data.addresses[0].total_sent,
+			};
 		});
 	}
 });
