67319902c51f9dbd36f502b589e30918bb8e708d
arcoard <arcoard+github@gmail.com>
2014-04-23 13:22:56 -0700

    Added sorting for each column, also changed date to be numerical so it can be properly sorted.

--- assets/js/lookup.js
+++ assets/js/lookup.js
@@ -36,7 +36,7 @@ lookupApp.controller('mainPageCtrl', function($scope, $http, ngTableParams, $fil
 				transactions[i] = {
 					'Hash' : data.txs[i]['hash'],
 					'Amount' : data.txs[i]['result'] / 100000000,
-					'Total Balance' : data.txs[i]['balance'] / 100000000,
+					'Balance' : data.txs[i]['balance'] / 100000000,
 					'InputAddress' : inputAddr,
 					'OutputAddress' : outputAddr,
 					'Date' : timeConverter(data.txs[i]['time'])
@@ -55,14 +55,16 @@ lookupApp.controller('mainPageCtrl', function($scope, $http, ngTableParams, $fil
 			var data = transactions;
 			$scope.tableParams = new ngTableParams({
 		        page: 1,            
-		        count: 5           // items per page
+		        count: 5,           // items per page
+		        sorting: {
+		        	Amount: 'desc' 
+		        }
 		    }, {
 		        total: transactions.length, 
 		        getData: function($defer, params) {
 		        	var data = transactions;
 		            var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
 		            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-		            console.log(orderedData);
 		        }
 		    });
 		}).
@@ -70,18 +72,6 @@ lookupApp.controller('mainPageCtrl', function($scope, $http, ngTableParams, $fil
 			$scope.loadError = true;
 		});
 	}
-	// $scope.tableParams = new ngTableParams({
- //        page: 1,            
- //        count: 5           // items per page
- //    }, {
- //        total: data.length, 
- //        getData: function($defer, params) {
- //        	var data = transactions;
- //            var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
- //            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
- //            console.log(orderedData);
- //        }
- //    });
 
 	$scope.getUSD = function(){
 		var url = 'https://blockchain.info/ticker?cors=true'
@@ -99,13 +89,13 @@ window.onload = function(){
 //Thanks StackOverflow
 function timeConverter(UNIX_timestamp){
  var a = new Date(UNIX_timestamp*1000); //Multiplied by 1000 because JS keeps time in miliseconds.  Unix time = seconds.
- var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+ var months = ['1','2','3','4','5','6','7','8','9','10','11','12'];
      var year = a.getFullYear();
      var month = months[a.getMonth()];
      var date = a.getDate();
      var hour = a.getHours();
      var min = a.getMinutes();
      var sec = a.getSeconds();
-     var time = date+', '+month+' '+year+' '+hour+':'+min+':'+sec ;
+     var time = year+'-'+month+'-'+date+ ' '+hour+':'+min+':'+sec ;
      return time;
  }
