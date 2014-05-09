05414c57f3fb9908b36c29673db227f61f09ee0b
arcoard <arcoard+github@gmail.com>
2014-05-09 14:09:49 -0700

    Switched view display to manual implementation.

--- assets/js/lookup.js
+++ assets/js/lookup.js
@@ -21,12 +21,13 @@ lookupApp.config(['$routeProvider',
 var data = [];
 lookupApp.controller('mainPageCtrl', function($scope, $http, $filter, $location, $routeParams, BlockchainService, $q){
 	$scope.Math = window.Math; //So absolute value can be called within bindings.
-	$scope.loaded = true;
+	$scope.loaded = false;
 	$scope.loading = false; //loaded != loading.  loading is for spinners, etc.
 	$scope.user = {addressID : '1gRPd4uauVLjHEFzyKohQaX9VK96awLFP'} //This is for debugging! Set the property to '' for prod.
 	var USD;
 	$scope.transactions = [];
 	$scope.myData = [];
+	$scope.visibleTab = 0;
 
 	// Deprecated code.  Removing soon. 
 	// $scope.lookupAddress = function (address) {
@@ -39,19 +40,25 @@ lookupApp.controller('mainPageCtrl', function($scope, $http, $filter, $location,
 	// };
 
 	$scope.callBlockchain = function(address) {
+		$scope.loaded = false;
+		$scope.loading = true;
 		var url = 'https://blockchain.info/multiaddr?cors=true&active='+address;
 		var promise = BlockchainService.lookupAddress(url);
 		promise.then(function(data){
-			$scope.myData = data;
+			$scope.myData = data.Transactions;
+			$scope.output = data;
 			$scope.columnsSelected = [{field:'Date', displayName:'Date'},
-									{field: 'Hash', displayName: 'Hash'}, 
+									{field: 'Hash', displayName: 'Hash', maxWidth: 5}, 
 									{field:'Amount', displayName:'Amount', cellTemplate: '<div ng-class="{green: row.getProperty(col.field) > 0, red: row.getProperty(col.field) < 0, }"><div class="ngCellText">{{row.getProperty(col.field)}}</div></div>'},
 									{field:'Balance', displayName:'Balance'}
 									];
-
+			$scope.loaded = true;
+			$scope.loading = false;
 		})
+
 	}
 
+	//TODO: Add to Account Overview
 	$scope.getUSD = function(){
 		var url = 'https://blockchain.info/ticker?cors=true'
 		$http.get(url).success(function(data){
@@ -75,10 +82,21 @@ lookupApp.controller('mainPageCtrl', function($scope, $http, $filter, $location,
         data: 'myData',
         selectedItems: $scope.mySelections,
         multiSelect: false,
+        keepLastSelected: false,
         plugins: [new ngGridFlexibleHeightPlugin()],
-        enablePinning: true,
+        showColumnMenu: true,
         columnDefs: 'columnsSelected'
     };
+
+    $scope.changeTab = function(tabNum){
+    	$scope.visibleTab = tabNum;
+    	//FIXME!!!
+    	//If data for the table is changed, and then the table tab is switched to, 
+    	//the data will not be displayed properly unless the grid has been prompted to redraw/sort.
+    	if (tabNum == 0){ //table tab.
+    		console.log($scope.gridOptions);
+    	}
+    }
 });
 
 lookupApp.controller('txController', function($scope, $http, $routeParams){
@@ -101,6 +119,8 @@ lookupApp.service('BlockchainService', ['$http', '$q', function($http, $q){
 	//Lookup Address is to be called for addresses only.  TODO: Another method for transactions and txController.
 	//Currently I'm refactoring so the function returns a promise.
 	var transactions = [];
+	var accountData = {};
+	// var output = {transactions: transactions, accountData: accountData}
 	var deferred = $q.defer();
 		return $http.get(url).then(function(data){
 			data = data.data; //original data var also includes header response.
@@ -122,23 +142,23 @@ lookupApp.service('BlockchainService', ['$http', '$q', function($http, $q){
 					'OutputAddress' : outputAddr,
 					'Date' : timeConverter(data.txs[i]['time'])
 				};
+
+				accountData = {
+					'BTC' : data.wallet.final_balance / 100000000, //Response in satoshi, so have to divide.
+					// 'Address' : address,
+					'Total Received': data.addresses[0].total_received / 100000000,
+					'Total Sent': data.addresses[0].total_sent / 100000000,
+					'Transactions' : transactions
+				};
 			};
 
 
-			deferred.resolve(transactions);
+			// deferred.resolve(transactions);
+			// return deferred.promise;
+
+			deferred.resolve(accountData);
 			return deferred.promise;
 
-			//Output is for general data, not for the tabular data.
-			//TODO: Implement a way to pass this info back to the controller without making a second HTTP request.
-			//Probably wrap the output in an array/obj, one is the below info, the other is the array of transactions.
-			// $scope.output = {
-			// 	'BTC' : data.wallet.final_balance / 100000000, //Response in satoshi, so have to divide.
-			// 	'Address' : address,
-			// 	'Total Received': data.addresses[0].total_received / 100000000,
-			// 	'Total Sent': data.addresses[0].total_sent / 100000000,
-			// 	'Transactions' : transactions
-			// };
-			
 		}, function(error){
 			console.log(error);
 		});
@@ -167,5 +187,3 @@ function timeConverter(UNIX_timestamp){
     location.href = "#"+h;
         history.replaceState(null,null,url)
 }
-
-
